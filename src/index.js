7805598fac6ca9d047527f72697db59f35634b21
Nick930826 <xianyou1993@qq.com>
2020-01-13 17:29:15 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import RouterMap from './Router';
-// import 'antd-mobile/dist/antd-mobile.css';
 
 ReactDOM.render(<RouterMap />, document.getElementById('root'));
